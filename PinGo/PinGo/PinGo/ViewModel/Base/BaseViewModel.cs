ad5a9f6fe93dc8852367e9699116cc87c6736be3
z003ny9s <z003ny9s@MD1Z5UBC>
2018-11-23 16:08:19 +0530

    Adding Project from 132.186.58.43

---
+++ PinGo/PinGo/PinGo/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using PropertyChanged;
+using System.ComponentModel;
+
+namespace PinGo
+{
+    /// <summary>
+    /// A base view model that fires Property Changed events as needed
+    /// </summary>
+    [AddINotifyPropertyChangedInterface]
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event that is fired when any child property changes its value
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+      /// <summary>
+      /// Used to fire the event <see cref="PropertyChanged"/> when the property with the <see cref="name"/> changes
+      /// </summary>
+      /// <param name="name">Name of the property</param>
+      public void OnPropertyChanged(string name)
+      {
+        PropertyChanged(this,new PropertyChangedEventArgs(name));
+      }
+    }
+}
---
+++ PinGo/PinGo/PinGo/ViewModel/WindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PinGo
+{
+  /// <summary>
+  /// The View model for the custom flat window
+  /// </summary>
+  public class WindowViewModel : BaseViewModel
+  {
+    #region Private Member
+    private Window mWindow;
+
+    #endregion
+
+    #region Constructor
+    public WindowViewModel(Window window)
+    {
+      mWindow = window;
+    }
+    #endregion
+  }
+}
